811b5b206125dd6ecb83d62340932c6a07307692
avasilev <avasilev23@yandex-team.ru>
2026-04-19 11:13:08 +0300

    Add runner vm in yandex cloud to run experiments

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -10,3 +10,13 @@ output "get_credentials_cmd" {
   description = "Run this to configure kubectl"
   value       = "yc managed-kubernetes cluster get-credentials ${yandex_kubernetes_cluster.k8s-cluster.name} --external --force"
 }
+
+output "runner_ip" {
+  description = "Public IP of the obs-bench runner VM"
+  value       = yandex_compute_instance.runner.network_interface[0].nat_ip_address
+}
+
+output "runner_ssh_cmd" {
+  description = "SSH command to connect to the runner VM"
+  value       = "ssh ubuntu@${yandex_compute_instance.runner.network_interface[0].nat_ip_address}"
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -67,3 +67,32 @@ variable "node_count" {
   type        = number
   default     = 1
 }
+
+variable "runner_vm_name" {
+  description = "Name of the obs-bench runner VM"
+  type        = string
+  default     = "obs-bench-runner"
+}
+
+variable "runner_cores" {
+  description = "Number of CPU cores for the runner VM"
+  type        = number
+  default     = 2
+}
+
+variable "runner_memory_gb" {
+  description = "RAM for the runner VM in GiB"
+  type        = number
+  default     = 4
+}
+
+variable "runner_disk_gb" {
+  description = "Boot disk size for the runner VM in GiB"
+  type        = number
+  default     = 20
+}
+
+variable "runner_ssh_public_key" {
+  description = "SSH public key for the runner VM (content of ~/.ssh/id_rsa.pub)"
+  type        = string
+}
